7bac262b3d9bb1f8e21cf6bab0724f7f8bb27481
unknown <habiburrahman5236@gmail.com>
2026-06-22 20:53:06 +0600

    explore menu done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
 
 const Home = () => {
+    const [category, setCategory] = useState("All")
     return (
         <div>
             <Header></Header>
+            <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
         </div>
     );
 };
 import './Home.css'
 import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 
 export default Home;
