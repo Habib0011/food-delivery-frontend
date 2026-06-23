6179852208c685d0c25a4cbe85f0e46d5083c51e
unknown <habiburrahman5236@gmail.com>
2026-06-23 21:32:20 +0600

    display food done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,11 +7,13 @@ const Home = () => {
         <div>
             <Header></Header>
             <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
+            <FoodDisplay category={category}></FoodDisplay>
         </div>
     );
 };
 import './Home.css'
 import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 
 export default Home;
