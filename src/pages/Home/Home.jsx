c0f99c24b1e2811d9488e2b2714b0950425e7be0
unknown <habiburrahman5236@gmail.com>
2026-06-27 17:47:31 +0600

    play store and app store done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import './Home.css'
+import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import AppDownload from '../../components/AppDownload/AppDownload';
+
 
 const Home = () => {
     const [category, setCategory] = useState("All")
@@ -8,12 +13,10 @@ const Home = () => {
             <Header></Header>
             <ExploreMenu category={category} setCategory={setCategory}></ExploreMenu>
             <FoodDisplay category={category}></FoodDisplay>
+            <AppDownload></AppDownload>
         </div>
     );
 };
-import './Home.css'
-import Header from '../../components/Header/Header';
-import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+
 
 export default Home;
